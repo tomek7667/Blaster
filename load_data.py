5c712b3a651fde2a76504a95bdbb8d6d317f9c94
Tomasz Dąbrowski <tomasz.dabrowski@carrier.com>
2023-10-24 11:22:59 +0200

    Add initial load of the data

---
+++ load_data.py
@@ -0,0 +1,5 @@
+import json
+
+def load_data(path="prepared/prepared_1697562094237.json"):
+    with open(path, "r") as f:
+        return json.load(f)
--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 import torch
-from databases import *
+from random import random
+from load_data import *
 
-databases = list_databases()
-print(databases)
-
-fasta = parse_fasta(open_database(databases[1]))
-
-dick = {}
-
-for fasta_record in fasta:
-    for word in fasta_record.class_name:
-        if not word in dick:
-            dick[word] = 1
-        else:
-            dick[word] += 1
-# sort descendingly both arrays based on results array (numerical)
-
-srtd = sorted(dick.items(), key=lambda x:x[1], reverse=True)
-
-for i in srtd[:10]:
-    print(i)
+max_chunked_sequence_length = 8
+split_coeff = 0.8
+print('loading data...')
+d = load_data()
+print('data loaded')
 
 
+def prepare_learn_and_test_set():
+    learn_data = []
+    test_data = []
+    for record in d:
+        class_name = record['c']
+        sequence = record['s']
+        for i in range(0, len(sequence), max_chunked_sequence_length):
+            chunked_sequence = sequence[i:i+max_chunked_sequence_length]
+            if len(chunked_sequence) < max_chunked_sequence_length:
+                break
+            if random() < split_coeff:
+                learn_data.append((class_name, chunked_sequence))
+            else:
+                test_data.append((class_name, chunked_sequence))
+
+    return learn_data, test_data
+
+def main():
+    print('preparing learn and test set...')
+    learn_data, test_data = prepare_learn_and_test_set()
+    print('learn and test set prepared')
+    learn_data_length = len(learn_data)
+    test_data_length = len(test_data)
+    total_data_length = learn_data_length + test_data_length
+    print(f"Learn data length: {learn_data_length}")
+    print(f"Test data length: {test_data_length}")
+    print(f"split coeff = {split_coeff}, {learn_data_length/total_data_length*100}%")
+    return
+
+if __name__ == "__main__":
+    main()
+    exit(0)
