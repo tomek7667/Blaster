3ad6d2dfc999caaa2833257215655634b5d3f930
Tomasz Dąbrowski <tomasz.dabrowski@carrier.com>
2023-10-31 15:06:13 +0100

    #1: Add zero padding in preprocessing

--- shorten_dataset.js
+++ shorten_dataset.js
@@ -1,12 +1,25 @@
 const { readFileSync, writeFileSync } = require("fs");
 
-const MAX_LENGTH = 3000;
+// max is the min of the number of items per class, so around 34
+const ITEMS_PER_CLASS = 20;
 
 const p = "prepared/prepared_1697562094237.json";
 const out = "prepared/prepared_1697562094237-short.json";
 
 const json = JSON.parse(readFileSync(p, "utf-8"));
 
-const outJson = json.slice(0, MAX_LENGTH);
-
+let dict = {};
+const outJson = json
+	.map((item) => {
+		const { c } = item;
+		if (dict[c] === undefined) {
+			dict[c] = 0;
+		}
+		if (dict[c] < ITEMS_PER_CLASS) {
+			dict[c]++;
+			return item;
+		}
+		return null;
+	})
+	.filter((item) => item !== null);
 writeFileSync(out, JSON.stringify(outJson));
