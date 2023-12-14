b749e21cfc5d297c677296a8ac9df4069c28e70d
tomek7667 <tomek.aabiot@gmail.com>
2023-12-14 01:17:44 +0100

    Add models, add new run with 0.69 f1 score

---
+++ models/Bf96435.py
@@ -0,0 +1,10 @@
+
+classes = ['Spirochaetes', 'Actinobacteria', 'Myxococcota', 'Planctomycetota', 'Fusobacteria', 'Fibrobacteres', 'Chlamydiae', 'Chlorobi', 'Tenericutes', 'Thermotogae', 'Deinococcus-Thermus', 'Pseudomonadota', 'Chloroflexi', 'Bacillota', 'Firmicutes', 'Aquificota', 'Proteobacteria', 'Fusobacteriota', 'Actinomycetota', 'Verrucomicrobia', 'Cyanobacteria', 'Mycoplasmatota', 'Campylobacterota', 'Bacteroidota']
+sequence_length = 700
+num_classes = 24
+dropout = 0.2
+a_size = 256
+b_size = 48
+batch_size = 16
+optimizer = "adam"
+learning_rate = 0.001
